261c0f708f0af4fd384ac5014523cb4c6820c4c0
Sacoya <Sacoyaengineer@gmail.com>
2023-01-26 12:57:04 -0500

    commit tree a57182fab4186c5409f0d189fb5c8e75ae668781

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki" ];
+function writeCards(cards) {
+    for( let i= 0; i < cards.length; i++)
+    console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+}
